ad0c6647f246306a052e632ef8314b3b34628b48
kelvinnzyoki <kelvinnzyokimaitha@gmail.com>
2026-04-15 16:15:13 +0300

    Update api.ts

--- lib/api.ts
+++ lib/api.ts
@@ -1,12 +1,9 @@
 export async function sendMessage(message: string) {
-  const res = await fetch("http://localhost:3000/ai/chat", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    credentials: "include",
-    body: JSON.stringify({ message }),
-  });
+  const API = "https://fit.cctamcc.site";
+
+fetch(`${API}/ai/chat`, {
+  credentials: "include"
+});
 
   return res.json();
 }
