e3dc5297dfe5516e57fbc475b30443ea653e044d
kelvinnzyoki <kelvinnzyokimaitha@gmail.com>
2026-04-15 14:55:49 +0300

    Create ChatInput.tsx

---
+++ components/chat/ChatInput.tsx
@@ -0,0 +1,16 @@
+import Input from "../ui/Input";
+import Button from "../ui/Button";
+
+export default function ChatInput({ input, setInput, sendMessage }: any) {
+  return (
+    <div className="p-3 border-t bg-white flex gap-2">
+      <Input
+        value={input}
+        onChange={(e: any) => setInput(e.target.value)}
+        placeholder="Ask your coach..."
+        onKeyDown={(e: any) => e.key === "Enter" && sendMessage()}
+      />
+      <Button onClick={sendMessage}>Send</Button>
+    </div>
+  );
+}
