bba4f6eb3082ed09642c11da4b3a6ddedfc2cff5
kelvinnzyoki <kelvinnzyokimaitha@gmail.com>
2026-04-16 03:58:22 +0300

    Update Input.tsx

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -1,8 +1,12 @@
-export default function Input(props: any) {
+import React from "react";
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+export default function Input({ className = "", ...props }: InputProps) {
   return (
     <input
       {...props}
-      className="border rounded-full px-4 py-2 w-full outline-none"
+      className={`border rounded-full px-4 py-2 w-full outline-none focus:ring-2 focus:ring-black ${className}`}
     />
   );
 }
