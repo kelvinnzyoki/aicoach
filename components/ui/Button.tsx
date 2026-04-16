695d0bd7b5bed9e02c51e5f55fcac17a1dfb5b07
kelvinnzyoki <kelvinnzyokimaitha@gmail.com>
2026-04-16 03:59:36 +0300

    Update Button.tsx

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,10 +1,5 @@
-export default function Button({ children, ...props }: any) {
-  return (
-    <button
-      {...props}
-      className="bg-black text-white px-4 py-2 rounded-full hover:opacity-80"
-    >
-      {children}
-    </button>
-  );
+import { redirect } from "next/navigation";
+
+export default function Home() {
+  redirect("/chat");
 }
